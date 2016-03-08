08b060640bf212366159bddeaab933eb810e4973
itomblack <itomblack@Ians-MacBook-Pro.local>
2016-03-08 12:16:29 +0000

    pre lunch commit

--- src/js/main.js
+++ src/js/main.js
@@ -9,44 +9,105 @@ var hexB = "00";
 var hslH = "360";
 var hslS = "100";
 var hslL = "100";
-var counter1 = 10;
+var counter1 = 60;
+
+var storeAnswers = [];
+
 
 //randomly create hex code
-var hexCode = '#' + (function co(lor){   return (lor +=
-  [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)])
-  && (lor.length == 6) ?  lor : co(lor); })('');
+getRandom();
+updatePageHex();
 
 //show hex code on home page
-// $('#hex-code').text(hexCode);
-// $('#timer-1').text(counter1.toString());
 
-//after timer, remove hex and show sliders
-// var timer = setInterval(function() {
-//     counter1--;
-//     if(counter1 < 0) {
-//         clearInterval(timer);
-//         $('#q-page').toggleClass('display-none');
-//         $('#a-page').toggleClass('display-none');
-//     } else {
-//       $('#timer-1').text(counter1.toString());
-//     }
-// }, 1000);
+$('#timer-1').text(counter1.toString());
 
+//after timer show end game screen
+var timer = setInterval(function() {
+    counter1--;
+    if(counter1 < 0) {
+        clearInterval(timer);
+        //calculate scores
+        $('#game-over').addClass('now-over');
+        //add to game over screen
+
+        //show game over screen
+    } else {
+      $('#timer-1').text(counter1.toString());
+    }
+}, 1000);
+
+
+// get initial values
+getHue();
+getSat();
+getLight();
+setColBack( hslH, hslS, hslL );
 
 //allow user to select HSL
 $('#slider-hue').change( function() {
-  console.log($('#slider-hue').val());
+    getHue();
+    setSatBack( hslH );
+    setLightBack( hslH );
+    setColBack( hslH, hslS, hslL );
 });
 
 $('#slider-sat').change( function() {
-  console.log($('#slider-sat').val());
+    getSat();
+    setColBack( hslH, hslS, hslL );
 });
 
 $('#slider-light').change( function() {
-  console.log($('#slider-light').val());
+    getLight();
+    setColBack( hslH, hslS, hslL );
 });
 
-//when ready get HSL values
+
+$('#btn-next-hex').click( function() {
+  //save current value
+  var something = "test";
+  storeAnswers.push( {'Q':hexCode, 'A': something} )
+
+  console.log(storeAnswers);
+
+
+  //show new one
+  getRandom();
+  updatePageHex();
+})
+
+//functions to change background sat & hue cols
+function getHue() { hslH = $('#slider-hue').val(); }
+function getSat() { hslS = $('#slider-sat').val(); }
+function getLight() { hslL = $('#slider-light').val(); }
+
+function setSatBack( hue ) {
+  var satBack = 'linear-gradient(to right, hsla(207, 0%, 50%, 1), hsla(' + hue + ', 100%, 50%, 1))'
+  $('#slider-sat-wrap').css('background', satBack);
+}
+
+function setLightBack( hue ) {
+  var lightBack = 'linear-gradient(to right, hsla(0, 0%, 0%, 1), hsla(' + hue + ', 100%, 50%, 1), hsla(360, 100%, 100%, 1))';
+  $('#slider-light-wrap').css('background', lightBack);
+}
+
+function setColBack( hue, sat, light ) {
+  $('#color-wrap').css('background', 'hsla(' + hue + ', ' + sat + '%, ' + light + '%, 1)');
+}
+
+//assign random hex code
+function getRandom() {
+  hexCode = '#' + (function co(lor){   return (lor +=
+    [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)])
+    && (lor.length == 6) ?  lor : co(lor); })('');
+}
+
+// update hex code on page
+function updatePageHex() {
+  $('#hex-code').text(hexCode);
+}
+
+//when ready get final HSL values
 
 //convert HSL to hex
 
